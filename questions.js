bd192e14e6321c79ad850e7f6dd75da5221dfa1c
Beenz <CH.SABEEN@GMAIL.COM>
2023-04-02 09:21:30 -0700

    questions stored in a separate js file

---
+++ questions.js
@@ -0,0 +1,53 @@
+var questions = [
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      "msg('Hello World')",
+      "alert('Hello World')",
+      "prompt('Hello World",
+      "alertBox('Hello World')",
+    ],
+    correct: "alert('Hello World')",
+  },
+
+  {
+    question: "Which of the following is not commonly used data types?",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    correct: "alerts",
+  },
+  {
+    quesitons:
+      "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correct: "parentheses",
+  },
+  {
+    quesitons:
+      "Which built-in method removes the last element from an array and returns that element?",
+    choices: ["last()", "get()", "pop()", "None of the Above"],
+    correct: "pop()",
+  },
+  {
+    quesitons:
+      "Which built-in method returns the calling string value converted to lower case?",
+    choices: [
+      "toLowerCase()",
+      "toLower()",
+      "changeCase(case)",
+      "None of the Above",
+    ],
+    correct: "toLowerCase()",
+  },
+  {
+    quesitons:
+      "Which of the following function of Number object returns the number's value",
+    choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+    correct: "valueOf()",
+  },
+  {
+    quesitons:
+      "Which of the following function of Array object joins all elements of an array into a string?",
+    choices: ["concat()", "join()", "pop()", "map()"],
+    correct: "join()",
+  },
+];
